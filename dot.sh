6092798a70c7884e5b7c2dceb13fd1a7d4b359ae
Herman Ekwall <herman@ekwall.dev>
2022-01-16 12:34:20 +0100

    Update dot.sh

--- dot.sh
+++ dot.sh
@@ -4,8 +4,8 @@
 
 cd ~
 git init
+git config user.email "herman@ekwall.dev"
 git remote add origin ssh://git@ssh.github.com:443/hekwall/dot.git
 git fetch
 git checkout main
 git submodule update --init --recursive
-git config user.email "herman@ekwall.dev"
