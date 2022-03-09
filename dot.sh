2cb8354ee96801cb8d70e4d99eef2f023cfc935c
Herman Ekwall <herman@ekwall.dev>
2022-03-09 12:23:26 +0100

    Update dot.sh

--- dot.sh
+++ dot.sh
@@ -2,6 +2,7 @@
 # Initialize dotfiles
 # curl -L https://blog.ekwall.dev/dot.sh | sh
 
+mkdir -p $HOME/.local/bin
 curl -fLo $HOME/.local/bin/yadm https://github.com/TheLocehiliosan/yadm/raw/master/yadm && chmod a+x $HOME/.local/bin/yadm
 cd $HOME
 $HOME/.local/bin/yadm clone -f ssh://git@ssh.github.com:443/hekwall/dot.git
