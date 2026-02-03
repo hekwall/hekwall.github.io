ba6f38be31e92ecf6be8f49823c003e8deddc03e
Herman Ekwall <herman@ekwall.dev>
2026-02-03 20:05:24 +0100

    Updates

--- dot.sh
+++ dot.sh
@@ -2,6 +2,6 @@
 # Initialize dotfiles
 # curl -L https://ekwall.dev/dot.sh | sh
 
-DEST="$HOME/.dot2"
+DEST="$HOME/.dot"
 [ -d $DEST ] || git clone git@github.com:hekwall/dot.git $DEST
 printf "Run: \n\tmake -C $DEST work\n\tmake -C $DEST home\n"
